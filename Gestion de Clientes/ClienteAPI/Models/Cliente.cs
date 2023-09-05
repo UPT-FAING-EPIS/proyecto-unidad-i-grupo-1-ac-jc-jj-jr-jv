a6fb322b4ea3dd71099b45c6f872fdc8e2fdb28a
JosueUPT <jaechambilla@upt.pe>
2023-09-05 10:38:55 -0500

    Entorno de Trabajo

---
+++ Gestion de Clientes/ClienteAPI/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClienteAPI.Models;
+
+public partial class Cliente
+{
+    public int IdCliente { get; set; }
+
+    public string NomCliente { get; set; } = null!;
+
+    public virtual ICollection<ClientesDocumento> ClientesDocumentos { get; set; } = new List<ClientesDocumento>();
+}
---
+++ Gestion de Clientes/ClienteAPI/Models/TiposDocumento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClienteAPI.Models;
+
+public partial class TiposDocumento
+{
+    public byte IdTipoDocumento { get; set; }
+
+    public string DesTipoDocumento { get; set; } = null!;
+
+    public virtual ICollection<ClientesDocumento> ClientesDocumentos { get; set; } = new List<ClientesDocumento>();
+}
